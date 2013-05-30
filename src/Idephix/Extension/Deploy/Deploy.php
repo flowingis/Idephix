56326e82a8435ad6191e0113dd933bbbfafbceaf
Manuel Kea Baldassarri <kea@keasoft.it>
2013-05-30 13:36:16 +0200

    Fix reduntant warmup.

--- src/Idephix/Extension/Deploy/Deploy.php
+++ src/Idephix/Extension/Deploy/Deploy.php
@@ -175,7 +175,7 @@ class Deploy implements IdephixAwareInterface
      */
     public function cacheClear()
     {
-        return $this->idx->remote('cd '.$this->getNextReleaseFolder().' && ./app/console cache:clear --env=prod --no-debug && ./app/console cache:warmup --env=prod', $this->dryRun);
+        return $this->idx->remote('cd '.$this->getNextReleaseFolder().' && ./app/console cache:clear --env=prod --no-debug', $this->dryRun);
     }
 
     /**
