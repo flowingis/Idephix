5b09e69fd4245a225cf17cae5af799d6716860e4
Danilo Sanchi <danilo.sanchi@gmail.com>
2014-06-16 21:50:52 +0200

    fix rm of not empty dir on sfDeploy

--- src/Idephix/Extension/Deploy/Deploy.php
+++ src/Idephix/Extension/Deploy/Deploy.php
@@ -176,7 +176,7 @@ class Deploy implements IdephixAwareInterface
                 try {
                     $this->idx->remote(
                         sprintf(
-                            "unlink %s || rmdir %s || rm %s",
+                            "unlink %s || rmdir -R %s || rm -R %s",
                             $fullPathReleaseSharedFile,
                             $fullPathReleaseSharedFile,
                             $fullPathReleaseSharedFile
