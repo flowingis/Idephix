4c521839bd9e631c113cae8953df74f2e238cf2f
Simone D'Amico <info@simonedamico.com>
2014-08-08 09:40:17 +0200

    Added custom port to rsync command

--- tests/Idephix/Extension/Project/ProjectTest.php
+++ tests/Idephix/Extension/Project/ProjectTest.php
@@ -17,10 +17,6 @@ class ProjectTest extends \PHPUnit_Framework_TestCase
              ->will($this->returnArgument(0));
 
         $this->idx->expects($this->exactly(1))
-             ->method('getCurrentTarget')
-             ->will($this->returnValue(new Config(array('ssh_params' => array('user' => 'kea')))));
-
-        $this->idx->expects($this->exactly(1))
              ->method('getCurrentTargetHost')
              ->will($this->returnValue('banana.com'));
 
@@ -31,9 +27,24 @@ class ProjectTest extends \PHPUnit_Framework_TestCase
 
     public function testRsyncProject()
     {
+        $this->idx->expects($this->exactly(1))
+          ->method('getCurrentTarget')
+          ->will($this->returnValue(new Config(array('ssh_params' => array('user' => 'kea')))));
+
         $result = $this->project->rsyncProject('/a/remote', './from');
 
         $this->assertEquals("rsync -rlDcz --force --delete --progress  -e 'ssh' ./from kea@banana.com:/a/remote/", $result);
+    }
+
+    public function testRsyncProjectWithCustomPort()
+    {
+        $this->idx->expects($this->exactly(1))
+          ->method('getCurrentTarget')
+          ->will($this->returnValue(new Config(array('ssh_params' => array('user' => 'kea', 'port' => 20817)))));
+
+        $result = $this->project->rsyncProject('/a/remote', './from');
+
+        $this->assertEquals("rsync -rlDcz --force --delete --progress  -e 'ssh -p 20817' ./from kea@banana.com:/a/remote/", $result);
     }
 
 }
