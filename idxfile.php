1d82220a27a7f10a72b88edd468acfa0866525ed
Michele Orselli <mo@ideato.it>
2015-06-04 12:00:30 +0200

    add release phar after successfull build

--- idxfile.php
+++ idxfile.php
@@ -35,25 +35,54 @@ $createPhar = function() use ($idx)
 
     if (false === strpos($out, 'Idephix version')) {
         echo "Error!\n";
-        exit(0);
+        exit(-1);
     }
 
     echo "\nAll good!\n";
 };
 
-$deployPhar = function () use ($idx) {
-    echo "Cloning website repository...\n";
-    $idx->local('rm -rf /tmp/getidephix && git clone git@github.com:ideatosrl/getidephix.com.git /tmp/getidephix/');
-    $idx->local('cp /tmp/Idephix/idephix.phar /tmp/getidephix/idephix.phar');
+$releasePhar = function() use ($idx) {
 
-    $version = $idx->local('cat /tmp/Idephix/.git/refs/heads/master');
-    $idx->local('cp /tmp/Idephix/.git/refs/heads/master /tmp/getidephix/version');
-    $idx->local(sprintf('cd /tmp/getidephix && git add . && git commit -m "Deploy phar version %s" && git push origin', $version));
+    $branch = getenv('TRAVIS_BRANCH');
+    $pr = getenv('TRAVIS_PULL_REQUEST');
 
-    echo "\nAll good!\n";
+    if ('master' != $branch || $pr) {
+        echo "skipping phar release branch $branch, PR $pr";
+        exit(0);
+    }
+
+    // decrypt and add key
+    $key = '$encrypted_b26b356be257_key';
+    $iv = '$encrypted_b26b356be257_iv';
+
+    $this->local('mkdir -p ~/.ssh');
+    $this->local("openssl aes-256-cbc -K $key -iv $iv -in ./id_rsa_idephix_doc.enc -out ~/.ssh/id_rsa_idephix_doc -d");
+    $this->local('chmod 600 ~/.ssh/id_rsa_idephix_doc');
+    $this->local('ssh-add ~/.ssh/id_rsa_idephix_doc');
+
+    // clone doc repo
+    $this->local('cd ~ && git clone --branch gh-pages git@github.com:ideatosrl/getidephix.com.git docs');
+    $this->local('cd ~/docs && git config user.name "ideatobot"');
+    $this->local('cd ~/docs && git config user.email "info@ideato.it"');
+
+    if (!file_exists('./idephix.phar')) {
+        echo 'Idephix phar does not exists';
+        exit(-1);
+    }
+
+    //copy new phar & commit
+    $this->local('cp -f idephix.phar ~/docs');
+    $this->local('cp ~/docs && git add -A .');
+    $this->local('cp ~/docs && git commit -qm "release new idephix version"');
+    $this->local('cp ~/docs && git push -q origin gh-pages');
+
+    // $version = $idx->local('cat /tmp/Idephix/.git/refs/heads/master');
+    // $idx->local(sprintf('cd /tmp/getidephix && git add . && git commit -m "Deploy phar version %s" && git push origin', $version));
+    // $idx->local('cp /tmp/Idephix/.git/refs/heads/master /tmp/getidephix/version');
 };
 
 $idx->add('createPhar', $createPhar);
+$idx->add('releasePhar', $releasePhar);
 $idx->add('buildTravis', $buildTravis);
 $idx->add('deployPhar', $deployPhar);
 $idx->add('build', $build);
