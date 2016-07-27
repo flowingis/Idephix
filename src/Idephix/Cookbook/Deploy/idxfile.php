45826c32c48690525f6a3347b89a659c9c95829e
Francesco Tassi <tassi.francesco@gmail.com>
2016-07-27 11:33:01 +0200

    Add dry-run to each command

--- src/Idephix/Cookbook/Deploy/idxfile.php
+++ src/Idephix/Cookbook/Deploy/idxfile.php
@@ -2,6 +2,8 @@
 
 function deploy(Idephix\IdephixInterface $idx, $go = false)
 {
+    $dryRun = !$go;
+    
     /** @var \Idephix\Context $target */
     $target = $idx->getCurrentTarget();
     $sharedFiles = $target->get('deploy.shared_files', array());
@@ -24,41 +26,44 @@ function deploy(Idephix\IdephixInterface $idx, $go = false)
         git fetch && \\
         git checkout --force {$deployBranch} && \\
         composer install --no-dev --prefer-dist --no-progress --optimize-autoloader --no-interaction
-    "
+    ",
+        $dryRun
     );
 
     $idx->remote(
         "mkdir -p {$remoteBaseDir}/releases && \\
-         mkdir -p {$remoteBaseDir}/shared"
+         mkdir -p {$remoteBaseDir}/shared",
+        $dryRun
     );
 
     foreach ($sharedFolders as $folder) {
-        $idx->remote("mkdir -p {$remoteBaseDir}/shared/{$folder}");
+        $idx->remote("mkdir -p {$remoteBaseDir}/shared/{$folder}", $dryRun);
     }
 
     foreach ($sharedFiles as $file) {
         $sharedFile = "{$remoteBaseDir}/shared/{$file}";
-        $idx->remote("mkdir -p `dirname '{$sharedFile}'` && touch \"$sharedFile\"");
+        $idx->remote("mkdir -p `dirname '{$sharedFile}'` && touch \"$sharedFile\"", $dryRun);
     }
 
     try {
-        $idx->remote("cd $remoteBaseDir && cp -pPR `readlink {$linkedRelease}` $nextRelease");
+        $idx->remote("cd $remoteBaseDir && cp -pPR `readlink {$linkedRelease}` $nextRelease", $dryRun);
     } catch (\Exception $e) {
         $idx->output()->writeln('<info>First deploy, sending the whole project</info>');
     }
 
-    $dryRun = $go ? '' : '--dry-run';
+    $extraOptions = $dryRun ? '--dry-run' : '';
 
-    $idx->rsyncProject($nextRelease, $localArtifact . '/', $rsyncExclude, $dryRun);
+    $idx->rsyncProject($nextRelease, $localArtifact . '/', $rsyncExclude, $extraOptions);
 
     foreach (array_merge($sharedFiles, $sharedFolders) as $item) {
-        $idx->remote("rm -r $nextRelease/$item");
-        $idx->remote("ln -nfs $remoteBaseDir/shared/$item $nextRelease/$item");
+        $idx->remote("rm -r $nextRelease/$item", $dryRun);
+        $idx->remote("ln -nfs $remoteBaseDir/shared/$item $nextRelease/$item", $dryRun);
     }
 
     $idx->remote(
         "
         cd $remoteBaseDir && \\
-        ln -nfs $nextRelease current"
+        ln -nfs $nextRelease current",
+        $dryRun
     );
 }
